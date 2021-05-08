b813be95969f9957ff2d709f781cdc03d91cae2a
анна <shesterikofff@mail.ru>
2021-05-08 21:11:27 +0300

    changed config to env vars

---
+++ Config.php
@@ -0,0 +1,16 @@
+<?php
+
+
+class Config
+{
+    public static function getCallbackApiConfirmationToken(): ?string
+    {
+        return getenv('CALLBACK_API_CONFIRMATION_TOKEN');
+    }
+
+    public static function getVkApiAccessToken(): ?string
+    {
+       return getenv('VK_API_ACCESS_TOKEN');
+    }
+
+}
