0a5c814168f605278dd821035363bd429fc66f3a
анна <shesterikofff@mail.ru>
2021-05-09 11:32:37 +0300

    changes

--- Logger.php
+++ Logger.php
@@ -8,7 +8,7 @@ class Logger
         $trace = debug_backtrace();
         $function_name = isset($trace[2]) ? $trace[2]['function'] : '-';
         $mark = date("H:i:s") . ' [' . $function_name . ']';
-        $log_name = '/logs/log_' . date("j.n.Y") . '.txt';
+        $log_name = 'logs/log_' . date("j.n.Y") . '.txt';
         file_put_contents($log_name, $mark . " : " . $message . PHP_EOL, FILE_APPEND);
     }
 
